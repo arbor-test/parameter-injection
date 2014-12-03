e9ac59a3b0344e5303d9e87ed27670a2a6c302c1
arbor-test <arborsolum@gmail.com>
2014-12-03 15:27:13 -0600

    Update unit_tests.sh

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -u $user:$password -H "Content-Type: application/json" -d '{"title": "$message"}' https://api.github.com/repos/arbor-test/parameter-injection/issues
+curl -i -X POST -u $user:$password -H "Content-Type: application/json" -d '{"title": "'"$message"'"}' https://api.github.com/repos/arbor-test/parameter-injection/issues
